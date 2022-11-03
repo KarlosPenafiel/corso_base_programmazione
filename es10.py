6d04ada467927ff389a7a729c2af5c739e7ec5cc
KarlosPenafiel <115649178+KarlosPenafiel@users.noreply.github.com>
2022-11-03 08:55:56 +0100

    Add files via upload

---
+++ es10.py
@@ -0,0 +1,12 @@
+i = 0
+while i < 1000000:
+        print(i)
+        i = i + 1
+
+        if (i==50):
+                break
+exit 
+lettera = input("press any key to exit")
+
+
+
---
+++ es2.py
@@ -0,0 +1,5 @@
+lato_quadrato = int(input("inserisci la lunghezza del lato del quadrato: "))
+perimetro = lato_quadrato * 4
+area = lato_quadrato * lato_quadrato
+print("Il perimeto del quadrato è: ", perimetro)
+print("L'area del quadrato è: ", area)
