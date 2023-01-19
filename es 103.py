bebedb641305c1b0eb1be43d4bd53cb681f384a0
KarlosPenafiel <115649178+KarlosPenafiel@users.noreply.github.com>
2023-01-19 08:18:59 +0100

    Create es 103.py

---
+++ es 103.py
@@ -0,0 +1,19 @@
+n = 0
+lung = 0
+i = 0
+somma = 0
+stringa = input("Inserisci una stringa numerica: ")
+lung = len(stringa)
+while i < counter:
+    n = stringa[i]
+    if(int(x)):
+        i = i + 1
+    else:
+        print("Inserisci solo numeri")
+        break
+i = 0
+while i < lung:
+    n = stringa[i]
+    somma = somma + int(x)
+    i = i + 1
+print(somma)
