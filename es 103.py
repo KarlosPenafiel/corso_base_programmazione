c14e7e1296c698c6e52d4d1eba587b672601edc5
KarlosPenafiel <115649178+KarlosPenafiel@users.noreply.github.com>
2023-01-19 08:26:55 +0100

    Update es 103.py

--- es 103.py
+++ es 103.py
@@ -2,14 +2,14 @@ n = 0
 lung = 0
 i = 0
 somma = 0
-stringa = input("Inserisci una stringa numerica: ")
+stringa = input("Inserisci una stringa di numeri: ")
 lung = len(stringa)
 while i < counter:
     n = stringa[i]
     if(int(x)):
         i = i + 1
     else:
-        print("Inserisci solo numeri")
+        print("Hai inserito una stringa")
         break
 i = 0
 while i < lung:
